0d9cb1156329b0c32b2f3c7e725b3e71d4ce1053
U-DESKTOP-O0QMAL6\johnb <johnblakeduffie@gmail.com>
2021-01-28 17:38:42 -0500

    config postgre

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn monstertracker.wsgi
+release: python manage.py migrate
+release: python manage.py makemigrations
--- monstertracker/settings.py
+++ monstertracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,12 +86,19 @@ WSGI_APPLICATION = 'monstertracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
+
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
