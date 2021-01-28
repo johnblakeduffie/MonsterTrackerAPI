a2a88d3788e6615d084394ef525fc8b2512a9dac
U-DESKTOP-O0QMAL6\johnb <johnblakeduffie@gmail.com>
2021-01-27 23:32:31 -0500

    fixed gunicorn

--- monstertracker/settings.py
+++ monstertracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'plh48=!qi_z-tl6k2@bl$)c-u9#xk+9*5t58#!sszho_*p6)++'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','54.163.200.183', 'monstertracker-api.herokuapp.com']
 
